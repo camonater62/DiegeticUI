d8baa4bc7ba9d1b6f85ef81af207b576eb686bba
Cameron West <camonater62@gmail.com>
2022-11-30 16:21:31 -0800

    Add note emitter

--- Assets/Scripts/Gremlin/GremlinManagerScript.cs
+++ Assets/Scripts/Gremlin/GremlinManagerScript.cs
@@ -11,17 +11,23 @@ public class GremlinManagerScript : MonoBehaviour
     //RER varies based on number of gremlins spawned
     //every time we add a gremlin, check if we have reached max, if so start climax
     public GameObject GremlinPrefab;
+    public ParticleSystem NoteEmitterPrefab;
     public GameObject[] Spawnpoints;
     public List<int> freeSpawns = new List<int>();
     public ScoreManager scoremng;
     public bool hardmode = false;
-    
+
+    private List<ParticleSystem> emitters = new List<ParticleSystem>();
+
     void Start()
     {
         // get number of spawnpoints
         for(int x = 0; x<Spawnpoints.Length; x++)
         {
             freeSpawns.Add(x);
+            emitters.Add(Instantiate(NoteEmitterPrefab));
+            emitters[x].transform.position = Spawnpoints[x].transform.position;
+            emitters[x].Stop();
         }
         elapsedTime = 0;
         totalTime = 0;
@@ -84,24 +90,18 @@ public class GremlinManagerScript : MonoBehaviour
         Random.InitState(System.DateTime.Now.Millisecond);
         int loc = Random.Range(0, freeSpawns.Count); // Random Range is exclusive for ints
         int loc2 = Random.Range(0, Spawnpoints[freeSpawns[loc]].transform.childCount);
-        // for (int i = 0; i < 10; i++){
-        //     Debug.Log(Random.Range(0, Spawnpoints[loc].transform.childCount-1));
-        // }
         string ret = "Spawn Loc: " + loc.ToString();
-        //ret += "\nSpawn Point child count: "+ Spawnpoints[loc].transform.childCount.ToString();
         ret += "\nSpawn Point point: "+ loc2.ToString();
-        // // ret += "\nCurr FreeSpawn: ";
-        // // for (int i = 0; i < freeSpawns.Count; i++){
-        // //     ret += freeSpawns[i].ToString();
-        // // }
         Debug.Log(ret);
 
 
         Transform selectedZone = Spawnpoints[freeSpawns[loc]].transform.GetChild(loc2);
 
         GameObject babyGremlin = Instantiate(GremlinPrefab, selectedZone.position, selectedZone.rotation); // Create gremlin instance and place it
+        emitters[loc].transform.position = selectedZone.position;
+        emitters[loc].Play();
+        
         babyGremlin.GetComponent<GremlinScript>().setSpawnNum(freeSpawns[loc]);
-        //babyGremlin.transform.Rotate(-90, 0, 0); // rotate gremlin so it is standing
         freeSpawns.RemoveAt(loc);
 
 
@@ -113,6 +113,7 @@ public class GremlinManagerScript : MonoBehaviour
     public void RemoveGobby(int num)
     {
         freeSpawns.Add(num);
+        emitters[num].Stop();
         DecrementRER();
         scoremng.IncrementScore();
     }
